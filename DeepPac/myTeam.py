938c5520fb306cf0944d520d51c9faa358de2a36
HeavenMin <ming1@student.unimelb.edu.au>
2017-10-13 00:14:00 +1100

    base

--- DeepPac/myTeam.py
+++ DeepPac/myTeam.py
@@ -1,10 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""
+ TEAM : DeepPac
+ PURPOSE : for pacman
+ VERSION : 1
+ DATE : 10.2017
+"""
+
+__author__ = 'DeepPac'
+
+
+
 # myTeam.py
 # ---------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -89,4 +103,3 @@ class DummyAgent(CaptureAgent):
     '''
 
     return random.choice(actions)
-
