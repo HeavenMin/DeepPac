77079493864f35630f65fb1350650fbb6bd6fd7f
HeavenMin <ming1@student.unimelb.edu.au>
2017-10-15 21:16:26 +1100

    fix a bug

--- DeepPac/DeepPac.py
+++ DeepPac/DeepPac.py
@@ -507,7 +507,6 @@ class DeepPacDefence(basicAgent):
          if isPacman(gameState, self.enemyIndexs[0]):
              e1Pos = getAgentPosition(gameState, self.enemyIndexs[0]) if getAgentPosition(gameState, self.enemyIndexs[0]) != None else self.enemyStartPosition
              waitInBound, _ = self.aStarSearch(gameState, position, self.homeEntries, [e1Pos])
-         waitInBound, _ = self.aStarSearch(gameState, position, self.homeEntries)
          elif isPacman(gameState, self.enemyIndexs[1]):
              e2Pos = getAgentPosition(gameState, self.enemyIndexs[1]) if getAgentPosition(gameState, self.enemyIndexs[1]) != None else self.enemyStartPosition
              waitInBound, _ = self.aStarSearch(gameState, position, self.homeEntries, [e2Pos])
